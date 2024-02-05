a8d4487988da38a0421c7307202c1ccda7541d5f
Болорболд Аригуун Эркевич <bolorboldariguun16@gmail.com>
2024-02-05 17:44:26 +0300

    Problem B solved

--- Introduction/Task B/Liseichikov's_Zoo.cpp
+++ Introduction/Task B/Liseichikov's_Zoo.cpp
@@ -3,6 +3,38 @@
 using namespace std;
 
 int main(){
-
+    string str;
+    cin >> str;
+    bool caught;
+    stack<char> st;
+    int animal = 0, trap = -1;
+    stack<int> animals, traps;
+    const int a = str.length() / 2;
+    int cage[a];
+    for(char c : str){
+        (islower(c)) ? animals.push(++animal) : traps.push(++trap);
+        if(st.empty()){
+            st.push(c);
+        } else {
+            caught = (isupper(st.top()) && islower(c) || islower(st.top()) && isupper(c))
+                   && tolower(st.top()) == tolower(c);
+            if(caught){
+                cage[traps.top()] = animals.top();
+                st.pop();
+                traps.pop();
+                animals.pop();
+            } else {
+                st.push(c);
+            }
+        }
+    }
+    if(st.empty()) {
+        cout << "Possible" << "\n";
+        for(int i = 0; i < a; i++){
+            cout << cage[i] << " ";
+        }
+    } else {
+        cout << "Impossible" << endl;
+    }
     return 0;
 }
