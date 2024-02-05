f5429324b18498b817681e2fdb662945df5197af
Болорболд Аригуун Эркевич <bolorboldariguun16@gmail.com>
2024-02-05 16:35:21 +0300

    Problem A solved

--- Introduction/Task A/Agronomist.cpp
+++ Introduction/Task A/Agronomist.cpp
@@ -1,8 +1,35 @@
-#include<bits/stdc++.h>
 #include<iostream>
 using namespace std;
 
 int main(){
-
+    int a;
+    int equal, mStart, mEnd, curStart, curEnd;
+    equal = mStart = mEnd = curStart = curEnd = 0;
+    cin >> a;
+    int x[a];
+    for(int i = 0; i < a; i++){
+        cin >> x[i];
+    }
+    for(int i = 0; i < a; i++){
+        if(x[i-1] == x[i]){
+            equal++;
+            if(equal == 3){
+                if(curEnd - curStart > mEnd - mStart){
+                    mEnd = curEnd;
+                    mStart = curStart;
+                }
+                curStart = i - 1;
+                equal = 2;
+            }
+        } else {
+            equal = 1;
+        }
+        curEnd = i;
+    }
+    if(curEnd - curStart > mEnd - mStart){
+        mEnd = curEnd;
+        mStart = curStart;
+    }
+    cout << mStart + 1 << " " << mEnd + 1 << endl;
     return 0;
 }
