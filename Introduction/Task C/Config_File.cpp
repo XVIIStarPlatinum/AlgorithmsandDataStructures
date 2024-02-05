20d59ba88b25a302896a9200790d852f1fc97ecd
Болорболд Аригуун Эркевич <bolorboldariguun16@gmail.com>
2024-02-05 18:40:17 +0300

    Problem D solved

--- Introduction/Task C/Config_File.cpp
+++ Introduction/Task C/Config_File.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(){
+    map<string, int> values;
 
     return 0;
 }
--- Introduction/Task D/Darwin_Nunez.cpp
+++ Introduction/Task D/Darwin_Nunez.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 
 int main(){
-
+    int a, b, c, d, k, tmp = 0;
+    cin >> a >> b >> c >> d >> k;
+    for(int i = 1; i <= k; i++) {
+        tmp = (a * b - c);
+        tmp = max(tmp, 0);
+        tmp = min(d, tmp);
+        if(tmp == a) break;
+        a = tmp;
+    }
+    cout << tmp << endl;
     return 0;
 }
