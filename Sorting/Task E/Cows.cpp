e0ed2e820b047edbe115d8dbd0f79bd543861c25
Болорболд Аригуун Эркевич <bolorboldariguun16@gmail.com>
2024-02-09 11:18:57 +0300

    Problem E solved

--- Sorting/Task E/Cows.cpp
+++ Sorting/Task E/Cows.cpp
@@ -1,3 +1,30 @@
-//
-// Created by Dell on 05.02.2024.
-//
+#include<iostream>
+using namespace std;
+
+bool checkPlace(int n, int k, const int arr[], int min){
+    int last = 0;
+    int placed = 1;
+    for(int i = 1; i < n; i++){
+        if(arr[i] - arr[last] >= min){
+            placed++;
+            last = i;
+            if(placed >= k) break;
+        }
+    }
+    return placed >= k;
+}
+int main() {
+    int n, k, curDist = 1;
+    cin >> n >> k;
+    int stall[n];
+    for(int i = 0; i < n; i++){
+        cin >> stall[i];
+    }
+    int maxDist = stall[n - 1] - stall[0];
+    while(curDist < maxDist - 1){
+        int m = (curDist + maxDist) / 2;
+        (checkPlace(n, k, stall, m)) ? curDist = m : maxDist = m;
+    }
+    (checkPlace(n, k, stall, maxDist)) ? cout << maxDist : cout << curDist;
+    return 0;
+}
