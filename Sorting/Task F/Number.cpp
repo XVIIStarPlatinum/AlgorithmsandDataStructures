76f293691023dc2f588d70cca99dfd26b29361b5
Болорболд Аригуун Эркевич <bolorboldariguun16@gmail.com>
2024-02-09 13:04:45 +0300

    Problem F solved

--- Sorting/Task F/Number.cpp
+++ Sorting/Task F/Number.cpp
@@ -1,3 +1,22 @@
-//
-// Created by Dell on 05.02.2024.
-//
+#include <bits/stdc++.h>
+using namespace std;
+
+bool comparator(const string &s1, const string &s2) {
+    return s1 + s2 > s2 + s1;
+}
+
+int main(){
+    vector<string> number;
+    string num;
+
+    while(cin >> num){
+        if(num.length() == 0) break;
+        number.push_back(num);
+    }
+
+    sort(number.begin(), number.end(), comparator);
+    for(const auto & i : number){
+        cout << i;
+    }
+    return 0;
+}
