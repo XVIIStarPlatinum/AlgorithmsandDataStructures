56e1c6c057e7b61a9a74512100c677ce90dbc270
Ariguun Bolorbold <bolorboldariguun16@gmail.com>
2024-04-19 16:56:25 +0300

    Proof for 1444 (with pic) + heapify (second attempt)

--- Sorting/heapify.cpp
+++ Sorting/heapify.cpp
@@ -2,18 +2,17 @@
 #include <iostream>
 #include <vector>
 
-int heapSize;
-
+int n;
 void heapify(int *arr, int i){
     int l = 2 * i + 1;
     int r = 2 * i + 2;
     int largest;
-    if(l <= heapSize && arr[l] > arr[i]){
+    if(l <= n && arr[l] > arr[i]){
         largest = l;
-    }else{
+    } else {
         largest = i;
     }
-    if(r <= heapSize && arr[r] > arr[largest]){
+    if(r <= n && arr[r] > arr[largest]){
         largest = r;
     }
     if(largest != i){
@@ -24,27 +23,53 @@ void heapify(int *arr, int i){
     }
 }
 
-void buildMaxHeap(int arr[], int n){
-    heapSize = n;
-    for(int i = n/2 - 1; i >= 0; i--){
+void heapify(std::vector<int>::iterator begin, int i) {
+    int largest;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    if(l <= n && begin[l] > begin[i]) {
+        largest = l;
+    } else {
+        largest = i;
+    }
+    if(r <= n && begin[r] > begin[largest]) {
+        largest = r;
+    }
+    if(largest != i) {
+        int tmp = begin[i];
+        begin[i] = begin[largest];
+        begin[largest] = tmp;
+        heapify(begin, largest);
+    }
+}
+void buildMaxHeap(int arr[], int sz) {
+    for(int i = sz / 2 - 1; i >= 0; i--) {
         heapify(arr, i);
     }
 }
-
-
+void buildMaxHeap(std::vector<int>::iterator begin, std::vector<int>::iterator end){
+    for(int i = (int)(end - begin) / 2 - 1; i >= 0; i--) {
+        heapify(begin, i);
+    }
+}
 
 int main() {
     int arr[] = {1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    n = sizeof(arr) / sizeof(arr[0]);
     std::cout << "Array to heap:\n";
     buildMaxHeap(arr, n);
-    std::vector<int> vec(arr, arr + n);
-
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i++) {
         std::cout << arr[i] << " ";
     }
+    std::vector<int> vec(arr, arr + n);
+    std::cout << "\nVector to heap (custom):\n";
+    n = (int) (vec.end() - vec.begin());
+    buildMaxHeap(vec.begin(), vec.end());
+    for(int i = 0; i < n; i++) {
+        std::cout << vec[i] << " ";
+    }
     std::cout << "\nVector to heap (make_heap):\n";
-    std::make_heap(vec.begin(), vec.end());
+      std::make_heap(vec.begin(), vec.end());
     for (int i : vec) {
         std::cout << i << " ";
     }
