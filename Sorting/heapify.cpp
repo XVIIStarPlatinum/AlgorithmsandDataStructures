f5ef12fee0d580c0aabd7f2123e15cd56315145e
Ariguun Bolorbold <bolorboldariguun16@gmail.com>
2024-05-03 11:22:09 +0300

    A schizophrenic nightmare
    Signed-off-by: Ariguun Bolorbold <bolorboldariguun16@gmail.com>

--- Sorting/heapify.cpp
+++ Sorting/heapify.cpp
@@ -1,47 +1,101 @@
 #include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <fstream>
 #include <iostream>
+#include <random>
 #include <vector>
 
-template <typename RAND_ACC_ITR>
-void heapify(RAND_ACC_ITR begin, RAND_ACC_ITR end, RAND_ACC_ITR current) {
+unsigned int randomGenerator(std::mt19937 &rng, unsigned int min, unsigned int max) {
+    rng.seed(std::random_device()());
+    std::uniform_int_distribution<std::mt19937::result_type> dist(min, max);
+    return dist(rng);
+}
 
-    RAND_ACC_ITR largest;
+std::vector<unsigned int> generate_vector(std::mt19937 &rng, unsigned int min, unsigned int max, size_t size){
+    std::vector<unsigned int> random_vector;
+    for(int i = 1; i < size; ++i) {
+        random_vector.push_back(randomGenerator(rng, min, max));
+    }
+    return random_vector;
+}
 
-    auto left = current + std::distance(begin, current);
-    auto right = left++;
+template <typename RandomAccessIterator>
+void heapify(RandomAccessIterator begin, RandomAccessIterator end, RandomAccessIterator current) {
 
-    if(left < end && *left > *current) {
-        largest = left;
+    RandomAccessIterator largest = current;
+
+    auto child1 = current + std::distance(begin, current);
+    auto child2 = child1++;
+
+    if(child1 < end && *current < *child1) {
+        largest = child1;
     } else {
         largest = current;
     }
-    if(right < end && *right > *largest) {
-        largest = right;
+    if(child2 < end && *largest < *child2) {
+        largest = child2;
     }
     if(largest != current) {
-        std::swap(*largest, *current);
+        std::swap(*current, *largest);
         heapify(current, end, largest);
     }
 }
 
-template <typename RAND_ACC_ITR>
-void buildMaxHeap(RAND_ACC_ITR begin, RAND_ACC_ITR end){
-    for(auto current = std::next(begin, std::distance(begin, end) / 2); current >= begin; current--) {
+template <typename RandomAccessIterator>
+void buildMaxHeap(RandomAccessIterator begin, RandomAccessIterator end){
+    for(auto current = std::next(begin, std::distance(begin, end) / 2) - 1; current >= begin; --current) {
         heapify(begin, end, current);
     }
 }
+void test_heapify(std::mt19937 &rng, unsigned int min, unsigned int max, size_t size) {
+    const char* path = R"(D:\ITMOrbius\A&DS_XVIIstarPt_\Sorting\test_results.csv)";
+    std::ofstream result_file;
+    result_file.open(path, std::ios::in | std::ios::app);
+    std::vector<unsigned int> vec1 = generate_vector(rng, min, max, size);
+    std::cout << "Randomly generated vector before being heapified:\n";
+    result_file << "Before\n";
+    for(unsigned int i : vec1) {
+        std::cout << i << " ";
+        result_file << i << ",";
+    }
+    result_file << "\n";
+    std::cout << "\nVector to heap (custom-made):\n";
+    result_file << "buildMaxHeap()\n";
+    const auto start1{std::chrono::steady_clock::now()};
+    buildMaxHeap(vec1.begin(), vec1.end());
+    const auto end1 {std::chrono::steady_clock::now()};
+    const std::chrono::duration<double> time_custom{end1 - start1};
+    for(unsigned int i : vec1) {
+        std::cout << i << " ";
+        result_file << i << ",";
+    }
+    std::cout << "\nElapsed time for custom function: " << time_custom.count();
+    result_file << "\nT1," << time_custom.count() << "\n";
+//    std::vector<int> vec2 = vec1;
+    std::cout << "\nVector to heap (make_heap):\n";
+    result_file << "make_heap()\n";
+    const auto start2 {std::chrono::steady_clock::now()};
+    std::make_heap(vec1.begin(), vec1.end());
+    const auto end2 {std::chrono::steady_clock::now()};
+    for (unsigned int i : vec1) {
+        std::cout << i << " ";
+        result_file << i << ",";
+    }
+    const std::chrono::duration<double> time_lib{end2 - start2};
+    std::cout << "\nElapsed time for make_heap(): " << time_lib.count() << "\n";
+    result_file << "\nT2," << time_lib.count() << "\n";
+//    assert(vec1 == vec2 && "Функция не прошла тестирование.");
+    result_file.flush();
+    result_file.close();
+}
 
 int main() {
-    std::vector<int> vec{1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17};
-    std::cout << "Vector to heap (custom-made):\n";
-    buildMaxHeap(vec.begin(), vec.end());
-    for(int i : vec) {
-        std::cout << i << " ";
-    }
-    std::cout << "\nVector to heap (make_heap):\n";
-    std::make_heap(vec.begin(), vec.end());
-    for (int i : vec) {
-        std::cout << i << " ";
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    for(int i = 1; i <= 3; ++i) {
+        std::cout << "TEST " << i << std::string(60, '=') << "\n";
+        test_heapify(rng, 5, 25, 25);
     }
     return 0;
 }
