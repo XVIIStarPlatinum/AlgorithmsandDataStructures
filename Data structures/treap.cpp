ef39e979873d36a7bd20b5657f7353ed1b51d6ce
Ariguun Bolorbold <bolorboldariguun16@gmail.com>
2024-05-03 14:05:57 +0300

    Of course, it ends with a segfault
    Signed-off-by: Ariguun Bolorbold <bolorboldariguun16@gmail.com>

--- Data structures/treap.cpp
+++ Data structures/treap.cpp
@@ -2,89 +2,123 @@
 #include <iostream>
 #include <random>
 
-int randomGenerator(size_t min, size_t max) {
-    std::mt19937 rng;
+int randomGenerator(std::mt19937 &rng, size_t min, size_t max) {
     rng.seed(std::random_device()());
     std::uniform_int_distribution<std::mt19937::result_type> dist(min, max);
     return dist(rng);
 }
+
 struct Node {
     int data;
     int priority;
     Node *left, *right;
-    explicit Node(int d) {
-        data = d;
-        priority = (int) randomGenerator(1, 100);
+    explicit Node(int d, int p) : data(d), priority(p) {
         left = nullptr;
         right = nullptr;
     }
 };
-class CartesianTree {
 
+class CartesianTree {
+    Node *root;
 public:
-    static void rotateLeft(Node *&root) {
-        Node *Right = root->right;
+    // Default constructor
+    CartesianTree(){
+        root = nullptr;
+    };
+    // Alternative constructor
+    explicit CartesianTree(Node *&root) : root(root) {}
+    // Destructor
+    ~CartesianTree() {
+        delete root;
+    }
+    // Copy constructor
+    CartesianTree(const CartesianTree& other) : root(other.root){
+        std::copy(other.root, other.root, root);
+    }
+    // Copy assignment constructor
+    CartesianTree& operator=(const CartesianTree& other) {
+        if(this != &other) {
+            delete root;
+            root = new Node(other.root->data, other.root->priority);
+        }
+        return *this;
+    }
+    // Move constructor
+    CartesianTree(CartesianTree&& other) noexcept : root(other.root){
+        other.root = nullptr;
+    }
+    // Move assignment constructor
+    CartesianTree& operator=(CartesianTree&& other) {
+        if(this != &other) {
+            delete root;
+            root = other.root;
+        }
+        return *this;
+    }
+
+    void rotateLeft() {
+        Node *r = root->right;
         Node *Key = root->right->left;
-        Right->left = root;
+        r->left = root;
         root->right = Key;
-        root = Right;
+        root = r;
     }
-    static void rotateRight(Node *&root) {
-        Node *Left = root->left;
+    void rotateRight() {
+        Node *l = root->left;
         Node *Priority = root->left->right;
-        Left->right = root;
+        l->right = root;
         root->left = Priority;
-        root = Left;
+        root = l;
     }
-    void insertNode(Node *&root, int d) {
-        if (root == nullptr) {
-            root = new Node(d);
+    void insertNode(int d, int p) {
+        if (!root) {
+            root = new Node{d, p};
         }
         else {
             if (d < root->data) {
-                insertNode(root->left, d);
+                insertNode(d, p);
                 if (root->left && root->left->priority > root->priority) {
-                    rotateRight(root);
+                    rotateRight();
                 }
             } else {
-                insertNode(root->right, d);
+                insertNode(d, p);
                 if (root->right && root->right->priority > root->priority) {
-                    rotateLeft(root);
+                    rotateLeft();
                 }
             }
         }
     }
-    bool searchNode(Node *root, int key) {
-        if (root == nullptr) {
+    bool searchNode(int key) {
+        if (!root) {
             return false;
         }
         if (root->data == key) {
             return true;
         }
         if (key < root->data) {
-            return searchNode(root->left, key);
+            return searchNode(key);
         }
-        return searchNode(root->right, key);
+        return searchNode(key);
     }
-    void deleteNode(Node *&root, int key) {
-        if (root == nullptr) {
+    void deleteNode(int key) {
+        if (!root) {
             return;
         }
         if (key < root->data) {
-            deleteNode(root->left, key);
+            deleteNode(key);
         } else if (key > root->data) {
-            deleteNode(root->right, key);
+            deleteNode(key);
         } else {
             if (root->left == nullptr && root->right == nullptr) {
                 delete root;
                 root = nullptr;
             } else if (root->left && root->right) {
                 if (root->left->priority < root->right->priority) {
-                    rotateLeft(root);
-                    deleteNode(root->left, key);
+                    rotateLeft();
+                    deleteNode(key);
                 } else {
-                    rotateRight(root);
-                    deleteNode(root->right, key);
+                    rotateRight();
+                    deleteNode(key);
                 }
             } else {
                 Node *child = (root->left) ? root->left : root->right;
@@ -110,26 +144,28 @@ public:
 };
 int main() {
     int nums[] = {1, 7, 6, 4, 3, 2, 8, 9, 10};
+    std::random_device dev;
+    std::mt19937 rng(dev());
     Node *root = nullptr;
     CartesianTree cartesianTree;
     for (int n: nums) {
-        cartesianTree.insertNode(root, n);
+        cartesianTree.insertNode(n, randomGenerator(rng, 1, 100));
     }
     std::cout << "Построенная дуча:\n\n";
     cartesianTree.displayTreap(root);
     std::cout << "\nДобавление узла 5:\n\n";
-    cartesianTree.insertNode(root, 5);
+    cartesianTree.insertNode(5, randomGenerator(rng, 1, 100));
     cartesianTree.displayTreap(root);
     std::cout << "\nУдаление узла 8:\n\n";
-    cartesianTree.deleteNode(root, 8);
+    cartesianTree.deleteNode(8);
     cartesianTree.displayTreap(root);
     std::cout << "\nУдаление узла 3:\n\n";
-    cartesianTree.deleteNode(root, 3);
+    cartesianTree.deleteNode(3);
     cartesianTree.displayTreap(root);
     std::cout << "\nПоиск всех узлов с 1 до 10:\n\n";
     for (int i = 1; i <= 10; i++) {
         std::cout << i << ": ";
-        (cartesianTree.searchNode(root, i)) ? std::cout << "есть" << "\n" : std::cout << "нет" << "\n";
+        (cartesianTree.searchNode(i)) ? std::cout << "есть" << "\n" : std::cout << "нет" << "\n";
     }
     return 0;
 }
